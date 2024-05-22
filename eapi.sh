7a67b04198cbc36f31d95225dad2e6e03805e321
ben <dfitz@me.com>
2024-05-22 00:07:02 -0400

    Update eapi.sh

--- eapi.sh
+++ eapi.sh
@@ -3,5 +3,7 @@
 OVERLAY_PATH="/var/db/repos/kde-sunset"
 
 # Update EAPI in ebuild files
-find $OVERLAY_PATH -name "*.ebuild" -exec sed -i 's/EAPI=[0-7]/EAPI=6/g' {} +
+find $OVERLAY_PATH -name "*.ebuild" -exec sed -i 's/EAPI=[0-7]/EAPI=8/g' {} +
 
+# Update EAPI in class files
+find $OVERLAY_PATH -name "*.eclass" -exec sed -i 's/EAPI=[0-7]/EAPI=8/g' {} +
