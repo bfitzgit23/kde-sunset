fdc1f697883a205d939ce00052f1692e74b35715
bfitzgit23 <dfitz@me.com>
2025-05-28 22:55:57 -0400

    switched to eapi 6

--- eapi.sh
+++ eapi.sh
@@ -3,7 +3,7 @@
 OVERLAY_PATH="/home/gentoo/Desktop/kde-sunset"
 
 # Update EAPI in ebuild files
-find $OVERLAY_PATH -name "*.ebuild" -exec sed -i 's/EAPI=[0-8]/EAPI=8/g' {} +
+find $OVERLAY_PATH -name "*.ebuild" -exec sed -i 's/EAPI=[0-6]/EAPI=6/g' {} +
 
 # Update EAPI in class files
-find $OVERLAY_PATH -name "*.eclass" -exec sed -i 's/EAPI=[0-8]/EAPI=8/g' {} +
+find $OVERLAY_PATH -name "*.eclass" -exec sed -i 's/EAPI=[0-6]/EAPI=6/g' {} +
